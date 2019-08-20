9eaa3014b472a48cb8a601b9785f9cce59f6aa0e
aline-camargo <aline97camargo@gmail.com>
2019-08-20 15:20:08 -0300

    primeiro esqueleto html, encodearea + decodearea

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,13 @@
+//arquivo para funcoes de cifrar e decifrar
 window.cipher = {
-  // ... 
+  encode: encode,
+  decode: decode
 };
+
+function encode(offset, string){
+
+}
+
+function decode(offset, string) {
+
+}
---
+++ src/index.js
@@ -0,0 +1,11 @@
+// arquivo para lidar com eventos do DOM (botoes e etc)
+// function clickAqui() {
+//  ex como chamar a funcao do cipher.js=
+//   window.cipher.encode();
+// }
+function encode(){
+  document.getElementById("encode_text_answer").innerText = "Código Cifrado";
+}
+function decode() {
+  document.getElementById('encode_text_answer').innerText = "Código Decifrado";
+}
