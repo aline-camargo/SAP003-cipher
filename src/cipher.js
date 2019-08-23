dd850d4848a7ea0bce197c2573128d1090c30319
aline-camargo <aline97camargo@gmail.com>
2019-08-23 17:58:58 -0300

    Hacker Edition Layout 2.1 testes ok 100%

--- src/cipher.js
+++ src/cipher.js
@@ -3,33 +3,33 @@ window.cipher = {
   decode: decode,
 };
 // Cifra mensagem c/ offset
-function encode(message, offset){
+function encode(offset, message) {
   let answerEncode = "";
   for (let index = 0; index < message.length; ++index) {
     let entryCharCode = message.charCodeAt(index);
-      if (entryCharCode >= 97 && entryCharCode <= 122) {
-        answerEncode += String.fromCharCode(((entryCharCode -97 + offset)%26) + 97);
-      } else if (entryCharCode >= 65 && entryCharCode <= 90){
-       answerEncode += String.fromCharCode(((entryCharCode -65 + offset)%26) + 65);
-     } else{
-       answerEncode += String.fromCharCode(entryCharCode);
-     }
+    if (entryCharCode >= 97 && entryCharCode <= 122) {
+      answerEncode += String.fromCharCode(((entryCharCode -97 + offset)%26) + 97);
+    } else if (entryCharCode >= 65 && entryCharCode <= 90) {
+      answerEncode += String.fromCharCode(((entryCharCode -65 + offset)%26) + 65);
+    } else {
+      answerEncode += String.fromCharCode(entryCharCode);
+    }
   }
   return answerEncode;
 }
 
 //Decifra mensagem c/ offset
-function decode(message, offset) {
+function decode(offset, message) {
   let answerDecode = "";
   for (let index = 0; index < message.length; ++index) {
     let entryCharCode = message.charCodeAt(index);
-      if (entryCharCode >= 97 && entryCharCode <= 122) {
-        answerDecode += String.fromCharCode(((entryCharCode -122 - offset)%26) + 122);
-      } else if (entryCharCode >= 65 && entryCharCode <= 90) {
-        answerDecode += String.fromCharCode(((entryCharCode -90 - offset)%26) + 90);
-      } else {
-        answerDecode += String.fromCharCode(entryCharCode);
-      }
+    if (entryCharCode >= 97 && entryCharCode <= 122) {
+      answerDecode += String.fromCharCode(((entryCharCode -122 - offset)%26) + 122);
+    } else if (entryCharCode >= 65 && entryCharCode <= 90) {
+      answerDecode += String.fromCharCode(((entryCharCode -90 - offset)%26) + 90);
+    } else {
+      answerDecode += String.fromCharCode(entryCharCode);
+    }
   }
   return answerDecode;
 }
--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
 // Arquivo para lidar com eventos do DOM (botões e etc)
 
 //Escutar botões de Codificar ou Decodificar e chamar suas respectivas funções.
-document.getElementById('encode-button').addEventListener("click", encodeChoice);
-document.getElementById('decode-button').addEventListener("click", decodeChoice);
+document.getElementById("encode-button").addEventListener("click", encodeChoice);
+document.getElementById("decode-button").addEventListener("click", decodeChoice);
 
 // Define as variáveis de mensagem e offset (chave de deslocamento) p/ serem usadas na cifra,
 // verifica se o valor do offset é negativo e corrige se for,
 // chama a função de cifrar a mensagem e imprime a resposta no DOM.
-function encodeChoice(){
-  let message = document.getElementById('text').value;
-  let offset = parseInt(document.getElementById('offset').value);
-    if (offset < 0) {
-      offset *= -1;
-    }
-  document.getElementById("text-answer").innerHTML = window.cipher.encode(message, offset);
-  return
+function encodeChoice() {
+  let message = document.getElementById("text-input").value;
+  let offset = parseInt(document.getElementById("offset").value);
+  if (offset < 0) {
+    offset *= -1;
+  }
+  document.getElementById("text-answer").innerHTML = window.cipher.encode(offset, message);
+  return;
 }
 
 // Define as variáveis de mensagem e offset (chave de deslocamento) p/ serem usadas na cifra,
 // verifica se o valor do offset é negativo e corrige se for,
 // chama a função de decifrar a mensagem e imprime a resposta no DOM.
 function decodeChoice() {
-  let message = document.getElementById('text').value;
-  let offset = parseInt(document.getElementById('offset').value);
-    if (offset < 0) {
-      offset *= -1;
-    }
-  document.getElementById("text-answer").innerHTML = window.cipher.decode(message, offset);
-  return
+  let message = document.getElementById("text-input").value;
+  let offset = parseInt(document.getElementById("offset").value);
+  if (offset < 0) {
+    offset *= -1;
+  }
+  document.getElementById("text-answer").innerHTML = window.cipher.decode(offset, message);
+  return;
 }
