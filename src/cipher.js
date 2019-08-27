ce3c2fdbc56450df68e360021530ff8d83688955
aline-camargo <aline97camargo@gmail.com>
2019-08-27 18:53:48 -0300

    Identação e Documentação OK - Atualização README.md, falta fluxograma

--- src/cipher.js
+++ src/cipher.js
@@ -1,35 +1,38 @@
+// Cifra mensagem c/ offset
+const encode = (offset, message) => {
+  //String vazia que irá conter mensagem cifrada.
+  let answerEncode = "";
+  for (let index = 0; index < message.length; ++index) {
+    //Verifica se o caractere é minúsculo, se sim converte de acordo com expressão e offset.
+    if (message.charCodeAt(index) >= 97 && message.charCodeAt(index) <= 122) {
+      answerEncode += String.fromCharCode(((message.charCodeAt(index) -97 + offset)%26) + 97);
+    //Verifica se o caractere é maiúsculo, se sim converte de acordo com expressão e offset.
+    } else if (message.charCodeAt(index) >= 65 && message.charCodeAt(index) <= 90) {
+      answerEncode += String.fromCharCode(((message.charCodeAt(index) -65 + offset)%26) + 65);
+    //Não altera caracteres de símbolos, números e espaço.
+    } else {
+      answerEncode += String.fromCharCode(message.charCodeAt(index));
+    }
+  }
+  return answerEncode;
+};
+
+//Decifra mensagem c/ offset. Mesmas operações da função encode, porém com o alfabeto e offset invertidos.
+const decode = (offset, message) => {
+  let answerDecode = "";
+  for (let index = 0; index < message.length; ++index) {
+    if (message.charCodeAt(index) >= 97 && message.charCodeAt(index) <= 122) {
+      answerDecode += String.fromCharCode(((message.charCodeAt(index) -122 - offset)%26) + 122);
+    } else if (message.charCodeAt(index) >= 65 && message.charCodeAt(index) <= 90) {
+      answerDecode += String.fromCharCode(((message.charCodeAt(index) -90 - offset)%26) + 90);
+    } else {
+      answerDecode += String.fromCharCode(message.charCodeAt(index));
+    }
+  }
+  return answerDecode;
+};
+
 window.cipher = {
   encode: encode,
   decode: decode,
 };
-// Cifra mensagem c/ offset
-function encode(offset, message) {
-  let answerEncode = "";
-  for (let index = 0; index < message.length; ++index) {
-    let entryCharCode = message.charCodeAt(index);
-    if (entryCharCode >= 97 && entryCharCode <= 122) {
-      answerEncode += String.fromCharCode(((entryCharCode -97 + offset)%26) + 97);
-    } else if (entryCharCode >= 65 && entryCharCode <= 90) {
-      answerEncode += String.fromCharCode(((entryCharCode -65 + offset)%26) + 65);
-    } else {
-      answerEncode += String.fromCharCode(entryCharCode);
-    }
-  }
-  return answerEncode;
-}
-
-//Decifra mensagem c/ offset
-function decode(offset, message) {
-  let answerDecode = "";
-  for (let index = 0; index < message.length; ++index) {
-    let entryCharCode = message.charCodeAt(index);
-    if (entryCharCode >= 97 && entryCharCode <= 122) {
-      answerDecode += String.fromCharCode(((entryCharCode -122 - offset)%26) + 122);
-    } else if (entryCharCode >= 65 && entryCharCode <= 90) {
-      answerDecode += String.fromCharCode(((entryCharCode -90 - offset)%26) + 90);
-    } else {
-      answerDecode += String.fromCharCode(entryCharCode);
-    }
-  }
-  return answerDecode;
-}
--- src/index.js
+++ src/index.js
@@ -1,26 +1,21 @@
 // Arquivo para lidar com eventos do DOM (botões e etc)
 
-//Escutar botões de Codificar ou Decodificar e chamar suas respectivas funções.
-document.getElementById("encode-button").addEventListener("click", encodeChoice);
-document.getElementById("decode-button").addEventListener("click", decodeChoice);
-
-// Define as variáveis de mensagem e offset (chave de deslocamento) p/ serem usadas na cifra,
-// verifica se o valor do offset é negativo e corrige se for,
-// chama a função de cifrar a mensagem e imprime a resposta no DOM.
-function encodeChoice() {
+//Função chamada quando o usuário aperta o botão de codificar.
+const encodeChoice = () => {
+  //Define as variáveis de mensagem e offset de acordo com o input do usuário.
   let message = document.getElementById("text-input").value;
   let offset = parseInt(document.getElementById("offset").value);
+  //Confere se a offset é negativa, se sim multiplica por -1 para torná-la positiva.
   if (offset < 0) {
     offset *= -1;
   }
+  //Chama a função de codificação e retorna seu resultado.
   document.getElementById("text-answer").innerHTML = window.cipher.encode(offset, message);
   return;
-}
+};
 
-// Define as variáveis de mensagem e offset (chave de deslocamento) p/ serem usadas na cifra,
-// verifica se o valor do offset é negativo e corrige se for,
-// chama a função de decifrar a mensagem e imprime a resposta no DOM.
-function decodeChoice() {
+//Função chamada quando o usuário aperta o botão de decodificar. Mesmas expressões da encodeChoice();, porém para decodificar.
+const decodeChoice = () => {
   let message = document.getElementById("text-input").value;
   let offset = parseInt(document.getElementById("offset").value);
   if (offset < 0) {
@@ -28,4 +23,8 @@ function decodeChoice() {
   }
   document.getElementById("text-answer").innerHTML = window.cipher.decode(offset, message);
   return;
-}
+};
+
+//Escutar botões de Codificar ou Decodificar do HTML e chamar suas respectivas funções.
+document.getElementById("encode-button").addEventListener("click", encodeChoice);
+document.getElementById("decode-button").addEventListener("click", decodeChoice);
