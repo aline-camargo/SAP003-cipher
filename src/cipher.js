b4c642c9d7dfbc4e01ab4774e2e766a4ec0f87ad
aline-camargo <aline97camargo@gmail.com>
2019-09-06 13:43:35 -0300

    Uso do for-of, declaração de variáveis para o if-else, correção eslint

--- src/cipher.js
+++ src/cipher.js
@@ -1,14 +1,16 @@
 // Cifra mensagem c/ offset
 const encode = (offset, message) => {
   let answerEncode = "";
-  for (let char of message){
-    let entryCharCode = message.charCodeAt(message.indexOf(char));
-    if (entryCharCode >= 97 && entryCharCode <= 122) {
+  for (let char of message) {
+    const entryCharCode = message.charCodeAt(message.indexOf(char));
+    const isLowerCase = entryCharCode >= 97 && entryCharCode <= 122;
+    const isUpperCase = entryCharCode >= 65 && entryCharCode <= 90;
+    if (isLowerCase) {
       //Define qual será a primeira letra do alfabeto considerando o sinal do offset.
-      let firstLetter = offset < 0 ? 122 : 97;
+      const firstLetter = offset < 0 ? 122 : 97;
       answerEncode += String.fromCharCode(((entryCharCode -firstLetter + offset)%26) + firstLetter);
-    } else if (entryCharCode >= 65 && entryCharCode <= 90) {
-      firstLetter = offset < 0 ? 90 : 65;
+    } else if (isUpperCase) {
+      const firstLetter = offset < 0 ? 90 : 65;
       answerEncode += String.fromCharCode(((entryCharCode -firstLetter + offset)%26) + firstLetter);
     } else {
       answerEncode += String.fromCharCode(entryCharCode);
@@ -20,13 +22,15 @@ const encode = (offset, message) => {
 //Decifra mensagem c/ offset. Mesmas operações da função encode, porém com o alfabeto e offset invertidos.
 const decode = (offset, message) => {
   let answerDecode = "";
-  for (let char of message){
-    let entryCharCode = message.charCodeAt(message.indexOf(char));
-    if (entryCharCode >= 97 && entryCharCode <= 122) {
-      let firstLetter = offset < 0 ? 97 : 122;
+  for (let char of message) {
+    const entryCharCode = message.charCodeAt(message.indexOf(char));
+    const isLowerCase = entryCharCode >= 97 && entryCharCode <= 122;
+    const isUpperCase = entryCharCode >= 65 && entryCharCode <= 90;
+    if (isLowerCase) {
+      const firstLetter = offset < 0 ? 97 : 122;
       answerDecode += String.fromCharCode(((entryCharCode -firstLetter - offset)%26) + firstLetter);
-    } else if (entryCharCode >= 65 && entryCharCode <= 90) {
-      firstLetter = offset < 0 ? 65 : 90;
+    } else if (isUpperCase) {
+      const firstLetter = offset < 0 ? 65 : 90;
       answerDecode += String.fromCharCode(((entryCharCode -firstLetter - offset)%26) + firstLetter);
     } else {
       answerDecode += String.fromCharCode(entryCharCode);
